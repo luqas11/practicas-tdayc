b092cde3d6a834213a5bdcb2b0b56178ba2ad99d
luqas11 <lucascazas@hotmail.com>
2025-09-09 19:21:26 -0300

    Ejercicios de la prácticaa 2

--- Práctica_2/Tarea_2/Tarea_2.ino
+++ Práctica_2/Tarea_2/Tarea_2.ino
@@ -5,11 +5,17 @@
 #include <Wire.h>
 
 #define PERIOD 20000 // período correspondiente a una frecuencia de 50Hz
+#define ALPHA 0.1f
 
 unsigned long t1;
 int counter = 0;
 
 Adafruit_MPU6050 mpu;
+
+float theta_g1 = 0;
+float theta_g = 0;
+float theta_a = 0;
+float theta = 0;
 
 void setup(void) {
   Serial.begin(115200);
@@ -28,63 +34,8 @@ void setup(void) {
   Serial.println("MPU6050 Found!");
 
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
   mpu.setFilterBandwidth(MPU6050_BAND_44_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
 
   Serial.println("");
   delay(100);
@@ -97,48 +48,44 @@ void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  if (counter == 10){
+  theta_g1 = theta_g1 + g.gyro.x * 0.02 * (180 / PI);
+  theta_a = atan2(a.acceleration.y, a.acceleration.z) * (180 / PI);
+
+  theta_g = theta + g.gyro.x * 0.02 * (180 / PI);
+  theta = ALPHA * theta_a + (1-ALPHA) * theta_g;
+
+  if (counter == 0){
     counter = 0;
-    matlab_send(a.acceleration.x, a.acceleration.y, a.acceleration.z, g.gyro.x, g.gyro.y, g.gyro.z);
+    matlab_send(theta_g, theta_a, theta);
   } else {
     counter++;
   }
 
   /* Print out the values */
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
+  Serial.print("Ángulo (giroscopio): ");
+  Serial.print(theta_g);
+  Serial.println("°");
 
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
+  Serial.print("Ángulo (acelerómetro): ");
+  Serial.print(theta_a);
+  Serial.println("°");
+
+  Serial.print("Ángulo (complementario): ");
+  Serial.print(theta);
+  Serial.println("°");
 
   Serial.println("");
   //delay(500);
   while (micros() < t1 + PERIOD){};
 }
 
-void matlab_send(float dato1, float dato2, float dato3, float dato4, float dato5, float dato6){
+void matlab_send(float dato1, float dato2, float dato3){
   Serial.write("abcd");
   byte * b = (byte *) &dato1;
   Serial.write(b,4);
   b = (byte *) &dato2;
   Serial.write(b,4);
   b = (byte *) &dato3;
-  Serial.write(b,4);
-  b = (byte *) &dato4;
-  Serial.write(b,4);
-  b = (byte *) &dato5;
-  Serial.write(b,4);
-  b = (byte *) &dato6;
   Serial.write(b,4);
   //etc con mas datos tipo float. Tambien podría pasarse como parámetro a esta funcion un array de floats.
 }
